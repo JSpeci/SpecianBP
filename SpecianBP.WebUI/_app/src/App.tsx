05f621ab9e2c2998f27d7ee6a5b25061bce1e78b
Your Name <you@example.com>
2019-03-22 12:06:33 +0100

    alive

--- SpecianBP.WebUI/_app/src/App.tsx
+++ SpecianBP.WebUI/_app/src/App.tsx
@@ -4,6 +4,9 @@ import { AppHeader } from './Components/AppHeader';
 import { Dashboard } from './Components/Dashboard';
 import { AppModel } from 'Models/AppModel';
 
+
+
+
 export interface AppProps {
   // router: AppRouter;
   appModel: AppModel;
@@ -13,6 +16,8 @@ export interface AppProps {
 export class App extends React.Component<AppProps> {
   render() {
     console.log("Rendering APP");
+
+    //ReactPDF.render(<mydoc />, `${__dirname}/pokusnyPDf.pdf`);
     return (
       <div className="container">
         <AppHeader />
