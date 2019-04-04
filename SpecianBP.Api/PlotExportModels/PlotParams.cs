de7dd09a4a9b6784f85e0e455ff72a0b90b02a7d
Your Name <you@example.com>
2019-04-04 12:12:56 +0200

    refactor - mapping serice created

---
+++ SpecianBP.Api/PlotExportModels/PlotParams.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SpecianBP.Api.PlotExportModels
+{
+    public class PlotParams
+    {
+        public string aggrFunc { get; set; }
+        public SeriesParams seriesParams { get; set; }
+        public ChartProps chartProps { get; set; }
+    }
+}
--- SpecianBP.WebUI/Controllers/SeriesController.cs
+++ SpecianBP.WebUI/Controllers/SeriesController.cs
@@ -10,6 +10,7 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using SpecianBP.Api;
 using SpecianBP.Api.Dto;
+using SpecianBP.Api.PlotExportModels;
 using SpecianBP.Db;
 using SpecianBP.Entities;
 using SpecianBP.Services;
@@ -25,11 +26,13 @@ namespace SpecianBP.WebUI.Controllers
 
         protected readonly DbService _dbService;
         protected readonly SeriesService _seriesService;
+        protected readonly MatplotLibParamsMappingService _matplotLibParamsMappingService;
 
-        public SeriesController(SeriesService seriesService, DbService dbService)
+        public SeriesController(DbService dbService, SeriesService seriesService, MatplotLibParamsMappingService matplotLibParamsMappingService)
         {
-            _seriesService = seriesService;
             _dbService = dbService;
+            _seriesService = seriesService;
+            _matplotLibParamsMappingService = matplotLibParamsMappingService;
         }
 
         /// <summary>
@@ -65,16 +68,11 @@ namespace SpecianBP.WebUI.Controllers
                 return BadRequest();
             }
 
-            ;
             string tempfolder = System.IO.Path.GetTempPath();
-            tempfolder = "C:\\Users\\King\\Documents\\BP\\ExampleSinPDF2222.pdf";
+            tempfolder = "C:\\Users\\King\\Documents\\BP\\";
             string pythonExe = "C:\\Users\\King\\AppData\\Local\\Programs\\Python\\Python37\\python.exe";
-            //string plotPath = "C:\\Users\\King\\source\\repos\\MatplotlibCS\\MatplotlibCS\\Python\\matplotlib_cs.py";
             string plotPath = "C:\\Users\\King\\source\\repos\\MatplotlibTest\\MatplotlibTest\\MatplotlibCS\\matplotlib_cs.py";
             var matplotLibEngine = new MatplotlibCS.MatplotlibCS(pythonExe, plotPath);
-
-            ;
-
 
             //query series service to get data
             //var result = _seriesService.GetAveraged(From, To, Step, SeriesName, MeasurementPlaceNumberId);
@@ -85,151 +83,11 @@ namespace SpecianBP.WebUI.Controllers
 
             //call matplotib and test it !
 
-            #region create test data
-
-            const int N = 100;
-            var X = new double[N];
-            var Y1 = new double[N];
-            var Y2 = new double[N];
-            var x = 0.0;
-            const double h = 2 * Math.PI / N;
-            var rnd = new Random();
-            for (var i = 0; i < N; i++)
-            {
-                var y = Math.Sin(x);
-                X[i] = x;
-                Y1[i] = y;
-
-                y = Math.Sin(2 * x);
-                Y2[i] = y + rnd.NextDouble() / 10.0;
-
-                x += h;
-            }
-
-            #endregion
-
-            #region create plot
-
-
-            var figure = new Figure(1, 1)
-            {
-                FileName = tempfolder,
-                OnlySaveImage = true,
-                DPI = 150,
-                Width = 1920,
-                Height = 1080,
-                Subplots =
-                {
-                    new Axes(1, "The X axis", "The Y axis")
-                    {
-                        Title = "Sin(x), Sin(2x), VLines, HLines, Annotations",
-                        LegendBorder = false,
-                        Grid = new Grid()
-                        {
-                            MinorAlpha = 0.2,
-                            MajorAlpha = 1.0,
-                            XMajorTicks = new[] {0.0, 7.6, 0.5, 1.75},
-                            YMajorTicks = new[] {-1, 2.5, 0.25, 0.125},
-                            XMinorTicks = new[] {0.0, 7.25, 0.25, 1.125},
-                            YMinorTicks = new[] {-1, 2.5, 0.125, 1.025}
-                        },
-                        PlotItems =
-                        {
-                            new Line2D("Sin")
-                            {
-                                X = X.Cast<object>().ToList(),
-                                Y = Y1.ToList(),
-                                LineStyle = LineStyle.Dashed
-                            },
-
-                            new Line2D("Sin 2x")
-                            {
-                                X = X.Cast<object>().ToList(),
-                                Y = Y2.ToList(),
-                                LineStyle = LineStyle.Solid,
-                                LineWidth = 0.5f,
-                                Color = "r",
-                                Markevery = 5,
-                                MarkerSize = 10,
-                                Marker = Marker.Circle,
-                                ShowLegend = false
-                            },
-
-                            new Text("ant1", "Text annotation", 4.5, 0.76)
-                            {
-                                FontSize = 17
-                            },
-
-                            new Annotation("ant2","Arrow text annotation", 0.5, -0.7, 3, 0)
-                            {
-                                Color = "#44ff88",
-                                ArrowStyle = ArrowStyle.Both,
-                                LineWidth = 3,
-                            },
-
-                            new Vline("vert line", new object[] {3.0}, -1, 1),
-                            new Hline("hrzt line", new[] {0.1, 0.25, 0.375}, 0, 5) {LineStyle = LineStyle.Dashed, Color = Color.Magenta}
-                        }
-                    }
-
-                }
-            };
-
+            var figure = _matplotLibParamsMappingService.getMatplotLibFigureFromPlotParams(plotParams, tempfolder + fileName);
             var t = matplotLibEngine.BuildFigure(figure);
             t.Wait();
-
-            #endregion
             Task.WaitAll(t);
-
             return Ok("exported");
-        }
-
-
-    }
-
-
-
-    public class MultilinePlotParams
-    {
-        public PlotParams[] plotParams { get; set; }
-    }
-
-    public class PlotParams
-    {
-        public string aggrFunc { get; set; }
-        public SeriesParams seriesParams { get; set; }
-        public ChartProps chartProps { get; set; }
-
-        public class SeriesParams
-        {
-            public DateTime from { get; set; }
-            public DateTime to { get; set; }
-            public Line line { get; set; }
-        }
-
-        public class Line
-        {
-            public string seriesName { get; set; }
-            public TimeSpan step { get; set; }
-            public int measurementPlaceNumberId { get; set; }
-        }
-
-        public class ChartProps
-        {
-            public string type { get; set; }
-            public int xSize { get; set; }
-            public int ySize { get; set; }
-            public string xAxisTitle { get; set; }
-            public string yAxisTitle { get; set; }
-            public int lineWidth { get; set; }
-            public LineColor lineColor { get; set; }
-        }
-
-        public class LineColor
-        {
-            public int r { get; set; }
-            public int g { get; set; }
-            public int b { get; set; }
         }
     }
 }
